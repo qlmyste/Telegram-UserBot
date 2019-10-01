3ffa605bfbb18e95c880f4118e047a132efca0bc
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2019-10-01 18:25:45 +0300

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -38,6 +38,8 @@ async def filter_incoming_handler(handler):
 @register(outgoing=True, pattern="^.filter (.*)")
 async def add_new_filter(new_handler):
     """ For .filter command, allows adding new filters in a chat """
+    try:
+        if not (await handler.get_sender()).bot:
             if not is_mongo_alive() or not is_redis_alive():
                 await handler.edit("`Database connections failing!`")
                 return
