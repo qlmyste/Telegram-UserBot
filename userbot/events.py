751527eb61bc8d6e2881ef5e094c905981e7d4ec
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2019-10-05 11:51:30 +0300

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -14,8 +14,9 @@ from time import gmtime, strftime
 from traceback import format_exc
 
 from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
 
-from userbot import bot, BOTLOG_CHATID
+from userbot import bot
 
 
 def register(**args):
@@ -24,10 +25,8 @@ def register(**args):
     disable_edited = args.get('disable_edited', False)
     ignore_unsafe = args.get('ignore_unsafe', False)
     unsafe_pattern = r'^[^/!#@\$A-Za-z]'
-    groups_only = args.get('groups_only', False)
-    trigger_on_fwd = args.get('trigger_on_fwd', False)
+    group_only = args.get('group_only', False)
     disable_errors = args.get('disable_errors', False)
-
     if pattern is not None and not pattern.startswith('(?i)'):
         args['pattern'] = '(?i)' + pattern
 
@@ -37,14 +36,11 @@ def register(**args):
     if "ignore_unsafe" in args:
         del args['ignore_unsafe']
 
-    if "groups_only" in args:
-        del args['groups_only']
+    if "group_only" in args:
+        del args['group_only']
 
     if "disable_errors" in args:
         del args['disable_errors']
-
-    if "trigger_on_fwd" in args:
-        del args['trigger_on_fwd']
 
     if pattern:
         if not ignore_unsafe:
@@ -52,25 +48,19 @@ def register(**args):
 
     def decorator(func):
         async def wrapper(check):
-            if not LOGSPAMMER:
-                send_to = check.chat_id
-            else:
-                send_to = BOTLOG_CHATID
-
-            if not trigger_on_fwd and check.fwd_from:
-                return
-
-            if groups_only and not check.is_group:
-                await check.respond("`I don't think this is a group.`")
+            if group_only and not check.is_group:
+                await check.respond("`Are you sure this is a group?`")
                 return
 
             try:
                 await func(check)
-
-            # Thanks to @kandnub for this HACK.
+            #
+            # HACK HACK HACK
             # Raise StopPropagation to Raise StopPropagation
             # This needed for AFK to working properly
-
+            # TODO
+            # Rewrite events to not passing all exceptions
+            #
             except events.StopPropagation:
                 raise events.StopPropagation
             # This is a gay exception and must be passed out. So that it doesnt spam chats
@@ -85,22 +75,20 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**USERBOT ERROR REPORT**\n"
-                    link = "[PaperplaneExtended Support Chat](https://t.me/PaperplaneExtendedSupport)"
-                    text += "If you want to, you can report it"
+                    text = "**Sorry, I encountered a error!**\n"
+                    link = "[https://t.me/userbot_support](Userbot Support Chat)"
+                    text += "If you wanna you can report it"
                     text += f"- just forward this message to {link}.\n"
-                    text += "Nothing is logged except the fact of error and date\n"
+                    text += "I won't log anything except the fact of error and date\n"
 
-                    ftext = "========== DISCLAIMER =========="
-                    ftext += "\nThis file uploaded ONLY here,"
-                    ftext += "\nwe logged only fact of error and date,"
-                    ftext += "\nwe respect your privacy,"
-                    ftext += "\nyou may not report this error if you've"
-                    ftext += "\nany confidential data here, no one will see your data\n"
-                    ftext += "================================\n\n"
-                    ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
+                    ftext = "\nDisclaimer:\nThis file uploaded ONLY here, "
+                    ftext += "we logged only fact of error and date, "
+                    ftext += "we respect your privacy, "
+                    ftext += "you may not report this error if you've "
+                    ftext += "any confidential data here, noone will see your data\n\n"
+                    ftext += "--------BEGIN USERBOT TRACEBACK LOG--------"
                     ftext += "\nDate: " + date
-                    ftext += "\nChat ID: " + str(check.chat_id)
+                    ftext += "\nGroup ID: " + str(check.chat_id)
                     ftext += "\nSender ID: " + str(check.sender_id)
                     ftext += "\n\nEvent Trigger:\n"
                     ftext += str(check.text)
@@ -110,9 +98,9 @@ def register(**args):
                     ftext += str(sys.exc_info()[1])
                     ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
 
-                    command = "git log --pretty=format:\"%an: %s\" -10"
+                    command = "git log --pretty=format:\"%an: %s\" -5"
 
-                    ftext += "\n\n\nLast 10 commits:\n"
+                    ftext += "\n\n\nLast 5 commits:\n"
 
                     process = await asyncsubshell(command,
                                                   stdout=asyncsub.PIPE,
@@ -127,15 +115,11 @@ def register(**args):
                     file.write(ftext)
                     file.close()
 
-                    if LOGSPAMMER:
-                        await check.client.respond(
-                            "`Sorry, my userbot has crashed.\
-                        \nThe error logs are stored in the userbot's log chat.`"
-                        )
-
-                    await check.client.send_file(send_to,
-                                                 "error.log",
-                                                 caption=text)
+                    await check.client.send_file(
+                        check.chat_id,
+                        "error.log",
+                        caption=text,
+                    )
                     remove("error.log")
             else:
                 pass
