e63e4f45b5065a31431291d523b7f830cc4a071b
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2019-10-06 17:14:16 +0300

    Update filter.py

--- userbot/modules/filter.py
+++ userbot/modules/filter.py
@@ -43,7 +43,8 @@ async def add_new_filter(event):
     if not is_mongo_alive() or not is_redis_alive():
         await event.edit("`Database connections failing!`")
         return
-    keyword = event.pattern_match.group(1)
+    message = event.text
+    keyword = message.split()
     string = event.text.partition(keyword)[2]
     msg = await event.get_reply_message()
     msg_id = None
