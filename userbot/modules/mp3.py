3fc267a0f014c8605a25febe0e5f3a41e6f407de
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-03-02 19:44:32 +0200

    Update mp3.py

--- userbot/modules/mp3.py
+++ userbot/modules/mp3.py
@@ -11,7 +11,7 @@ async def mp3(e):
       await e.edit("**Bot doesn't support magic! Use voice message.**", delete_in=3)
       return
   file = await bot.download_file(file)
-  await e.client.send_file(e.chat.id,
+  await e.client.send_file(e.chat_id,
                            f'voice.mp3',
                            reply_to=reply_message)
   os.remove(f'voice.mp3')
