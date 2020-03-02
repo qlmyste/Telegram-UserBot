b538fe86cf66503ea3a3842435bed24c825f97f0
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-03-02 19:21:30 +0200

    Update mp3.py

--- userbot/modules/mp3.py
+++ userbot/modules/mp3.py
@@ -8,11 +8,13 @@ async def mp3(e):
   message = await e.get_reply_message()
   file = message.audio or message.voice
   if not file:
-      await e.edit("**No audio file specified**", delete_in=3)
+      await e.edit("**Bot doesn't support magic! Use voice message.**", delete_in=3)
       return
   file = await bot.download_file(file)
-  await e.client.send_file(e.chat.id, f'{voice.mp3}',",reply_to=reply_message)
-  os.remove(f'{voice.mp3}')
+    await e.client.send_file(e.chat.id,
+                              f'voice.mp3',
+                              reply_to=reply_message)
+  os.remove(f'voice.mp3')
   CMD_HELP.update({
     ".mp3":
     "Convert a voice message to a mp3 and send it."
