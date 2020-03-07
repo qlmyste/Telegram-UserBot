34630f14b545f0d7ca9bce8f392f05903bdff923
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-03-08 00:19:37 +0200

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -47,7 +47,7 @@ async def img_sampler(event):
     os.system("./bing.py -nn -l " + lim + " -u https://www.bing.com/images/search?q=" + query)
     
     for filename in os.listdir("downloads"):
-      paths = 'downloads/' + filename ', '
+      paths = 'downloads/' + filename + ', '
     await event.client.send_file(event.chat.id, paths)
     rmtree("downloads")
     await event.delete()
