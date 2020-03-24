e52d50eb73adce6b2a58a80323128e577c3d2ddb
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-03-24 20:59:33 +0200

    Update mp3.py

--- userbot/modules/mp3.py
+++ userbot/modules/mp3.py
@@ -1,6 +1,7 @@
 from userbot import BOTLOG, bot, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 from userbot.utils import parse_arguments
+from moviepy import editor as mp
 import os
 
 @register(outgoing=True, pattern=r"^\.mp3$")
@@ -10,13 +11,27 @@ async def mp3(e):
   if not file:
       await e.edit("**Bot doesn't support magic! Use voice message.**")
       return
+  video = message.video
+  if not video
+      await e.edit("**Bot doesn't support magic! Use video message.**")
+      return
   await e.edit("**Downloading...**")
-  file = await bot.download_file(file, "voice.mp3")
-  await e.edit("**Sending...**")
-  await e.client.send_file(e.chat_id,
-                           f'voice.mp3',
-                           reply_to=message)
-  os.remove(f'voice.mp3')
+  if file:
+    file = await bot.download_file(file, "voice.mp3")
+    await e.edit("**Sending...**")
+    await e.client.send_file(e.chat_id,
+                            f'voice.mp3',
+                            reply_to=message)
+    os.remove(f'voice.mp3')
+  if video:
+    video = await bot.download_file(video, "video.mp4")
+    await e.edit("**Converting video...**")
+    clip = mp.VideoFileClip('video.mp4')
+    clip.audio.write_audiofile(f'video.mp3')
+    await e.edit("**Sending mp3...**")
+    await e.client.send_file(e.chat.id,
+                              f'video.mp3',
+                              reply_to=reply_message)
   CMD_HELP.update({
     ".mp3":
     "Convert a voice message to a mp3 and send it."
