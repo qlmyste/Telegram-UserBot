ca30f3c98826d9ba68737a783ffd58d4733546b9
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-03-24 21:15:41 +0200

    Update mp3.py

--- userbot/modules/mp3.py
+++ userbot/modules/mp3.py
@@ -16,17 +16,17 @@ async def mp3(e):
                             reply_to=message)
     os.remove(f'voice.mp3')
    if message.video:
-       video = message.video
-       await e.edit("**Downloading...**")
-       video = await bot.download_file(video, "video.mp4")
-       await e.edit("**Converting video...**")
-       clip = mp.VideoFileClip('video.mp4')
-       clip.audio.write_audiofile(f'video.mp3')
-       await e.edit("**Sending mp3...**")
-       await e.client.send_file(e.chat.id,
-                                f'video.mp3',
-                                reply_to=reply_message)
-     else:
+    video = message.video
+    await e.edit("**Downloading...**")
+    video = await bot.download_file(video, "video.mp4")
+    await e.edit("**Converting video...**")
+    clip = mp.VideoFileClip('video.mp4')
+    clip.audio.write_audiofile(f'video.mp3')
+    await e.edit("**Sending mp3...**")
+    await e.client.send_file(e.chat.id,
+                             f'video.mp3',
+                             reply_to=reply_message)
+   else:
          await e.edit("**Bot doesn't support magic! Use video or voice message.**")
          return
   CMD_HELP.update({
