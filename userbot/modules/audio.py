cc1de0a87fa70b130a3c79b4f0a757d140b326b5
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-04-02 16:10:40 +0300

    Update audio.py

--- userbot/modules/audio.py
+++ userbot/modules/audio.py
@@ -13,13 +13,13 @@ async def audiotag(at):
   message = await at.get_reply_message()
   if message.audio or message.voice:
     file = message.audio or message.voice
-    at.edit("Getted audio file")
+    await at.edit("Getted audio file")
     file = await bot.download_file(file, "audio.mp3")
-    at.edit("Downloaded")
-    os.system("ffmpeg -i audio.mp3 -ar 8000 -ac 1 -vn converted.wav")
-    at.edit("Converted")
-    payload = {'action': 'identify', 'apikey': API_CODE}
-    at.edit("Payload complete")
-    result = requests.post(api_url,data=payload,files={'file': open(filename, 'rb')})
-    at.edit("Result getted")
-    at.edit(result.text)
+    await at.edit("Downloaded")
+    await os.system("ffmpeg -i audio.mp3 -ar 8000 -ac 1 -vn converted.wav")
+    await at.edit("Converted")
+    await payload = {'action': 'identify', 'apikey': API_CODE}
+    await at.edit("Payload complete")
+    await result = requests.post(api_url,data=payload,files={'file': open(filename, 'rb')})
+    await at.edit("Result getted")
+    await at.edit(result.text)
