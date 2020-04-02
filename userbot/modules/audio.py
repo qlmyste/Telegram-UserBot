7264aca3cc53340155abb42ca275ccf246ee78e5
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-04-02 16:57:51 +0300

    Update audio.py

--- userbot/modules/audio.py
+++ userbot/modules/audio.py
@@ -8,20 +8,22 @@ api_url = 'https://audiotag.info/api'
 @register(outgoing=True, pattern=r"^\.audio$")
 async def audiotag(at):
   if API_CODE is None:
-    await at.edit("We don't support magic! No API Code! Take it from audiotag.info")
+    await at.edit("**We don't support magic! No API Code! Take it from audiotag.info**")
     return
   message = await at.get_reply_message()
   if message.audio or message.voice:
     file = message.audio or message.voice
-    await at.edit("Getted audio file")
+    await at.edit("**Getted audio file**")
+    await at.edit("**Downloading**")
     file = await bot.download_file(file, "audio.mp3")
-    await at.edit("Downloaded")
+    await at.edit("**Downloaded**")
+    await at.edit("**Converting**")
     os.system("ffmpeg -i audio.mp3 -ar 8000 -ac 1 -vn converted.wav")
-    await at.edit("Converted")
+    await at.edit("**Converted**")
     payload = {'action': 'identify', 'apikey': API_CODE}
-    await at.edit("Payload complete")
+    await at.edit("**Payload complete**")
     result = requests.post(api_url,data=payload,files={'file': open('converted.wav', 'rb')})
-    await at.edit("Result getted")
+    await at.edit("**Result getted**")
     await at.edit(result.text)
     result_object = json.loads(result.text)
     if result_object['success']==True and result_object['job_status']=='wait':
@@ -47,7 +49,7 @@ async def audiotag(at):
           os.remove('audio.mp3')
           break;
       else:
-        await at.edit("Sorry, i can't recognize it.")
+        await at.edit("**Sorry, i can't recognize it.**")
         os.remove('converted.wav')
         os.remove('audio.mp3')
 
