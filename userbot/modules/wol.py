5fc04b316d252cd3d7d3435aabb0e5571105fd8b
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-04-12 18:41:48 +0300

    Update wol.py

--- userbot/modules/wol.py
+++ userbot/modules/wol.py
@@ -1,10 +1,12 @@
 from userbot.events import register
 from userbot import MAC_ADDRESS as MAC
-
+from userbot import BOTLOG, bot, BOTLOG_CHATID, CMD_HELP
+from wakeonlan import send_magic_packet
 @register(outgoing=True, pattern=r"^\.boot$")
 async def boot(bt):
     if MAC is None:
       await bt.edit("**We don't support magic! No API Code! Take it from audiotag.info**")
       return
+    send_magic_packet(MAC)
     
-    
+CMD_HELP.update({"boot": "Boot your PC on local server with MAC, given in conf file."})
