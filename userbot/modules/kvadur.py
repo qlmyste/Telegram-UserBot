c3cf4686cf716232b943d5eac6564d1c594cbd07
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-05-08 21:44:34 +0300

    Update kvadur.py

--- userbot/modules/kvadur.py
+++ userbot/modules/kvadur.py
@@ -1,7 +1,7 @@
 import asyncio
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
-import re
+from math import sqrt
 @register(outgoing=True, pattern="^.kvadur(?: |$)(.*)")
 async def evaluate(kvad):
       if kvad.pattern_match.group(1):
@@ -22,8 +22,13 @@ async def evaluate(kvad):
             i += 1
             if s_int != '':
               integ.append(int(s_int))
-        m = integ[0] + 3
-        await kvad.edit("Test")
+        a = integ[0]
+        b = integ[1]
+        c = integ[2]
+        D = b*b - 4*a*c
+        x1 = (-b - sqrt(D))/2*a
+        x2 = (-b + sqrt(D))/2*a
+        await kvad.edit("`\n**Result: **\n`\n`D = `f"{D}"`\n **x1 = ** `f"{x1}"`, "**x2 = ** `f"{x2}"`")
       else:
         await kvad.edit("``` Give a numbers to evaluate. ```")
         return
