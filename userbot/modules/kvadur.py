e20558355b87264ecda850cba481a2c34152bca0
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-05-08 19:48:06 +0300

    Update kvadur.py

--- userbot/modules/kvadur.py
+++ userbot/modules/kvadur.py
@@ -4,9 +4,9 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.kvadur(?: |$)(.*)")
 async def evaluate(kvad):
-      if query.pattern_match.group(1):
-        expression = query.pattern_match.group(1)
-        await query.edit(expression)
+      if kvad.pattern_match.group(1):
+        expression = kvad.pattern_match.group(1)
+        await kvad.edit(expression)
       else:
-        await query.edit("``` Give a numbers to evaluate. ```")
+        await kvad.edit("``` Give a numbers to evaluate. ```")
         return
