b08a0f5d85fdad4909decafff00a8e460b5baec2
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-05-08 20:04:17 +0300

    Update kvadur.py

--- userbot/modules/kvadur.py
+++ userbot/modules/kvadur.py
@@ -6,7 +6,7 @@ import re
 async def evaluate(kvad):
       if kvad.pattern_match.group(1):
         expression = kvad.pattern_match.group(1)
-        numbers = re.findall(r'\b\d+\b', 'he33llo 42 I\'ma 32 string 30')
+        numbers = re.findall(r'\b\d+\b', expression)
         await kvad.edit(numbers.groups())
       else:
         await kvad.edit("``` Give a numbers to evaluate. ```")
