8e4b44506249a81bf1e9ea41e6ed4726b4b158a5
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-05-08 19:41:37 +0300

    Create kvadur.py

---
+++ userbot/modules/kvadur.py
@@ -0,0 +1,12 @@
+import asyncio
+from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.kvadur(?: |$)(.*)")
+async def evaluate(kvad):
+      if query.pattern_match.group(1):
+        expression = query.pattern_match.group(1)
+        await query.edit(expression)
+    else:
+        await query.edit("``` Give a numbers to evaluate. ```")
+        return
