68e3f8d08a8b191b5e1e38bcc304f1020d8f7164
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-05-08 19:55:22 +0300

    Update kvadur.py

--- userbot/modules/kvadur.py
+++ userbot/modules/kvadur.py
@@ -1,14 +1,19 @@
 import asyncio
 from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
-import re
 
 @register(outgoing=True, pattern="^.kvadur(?: |$)(.*)")
 async def evaluate(kvad):
       if kvad.pattern_match.group(1):
         expression = kvad.pattern_match.group(1)
-        numbers = re.findall('(\d+)', expression)
-        await kvad.edit(numbers)
+        list_num = []
+        for i in expression:
+            try:
+                   num = int(i)
+                   list_num.append(num)
+            except ValueError:
+                  continue
+        await kvad.edit(list_num)
       else:
         await kvad.edit("``` Give a numbers to evaluate. ```")
         return
