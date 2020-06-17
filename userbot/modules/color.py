5c0cecccc1cead4a5dbf1a97accb3ba7175c498a
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-06-17 21:36:11 +0300

    Update color.py

--- userbot/modules/color.py
+++ userbot/modules/color.py
@@ -85,13 +85,8 @@ def make_swatch(color, size=(300, 128)):
     color_swatch.save(output, format="PNG")
     return output.getvalue()
 
-
-CMD_HELP.update({
-    "color":
-    "Show the supplied color and return it \
-    \nin different colorspaces.\
-    \n\n.color [options] (color)\
-    \n\nOptions:\
-    \n`.format`: Format of the supplied color. Defaults to `html` which can be any valid HTML color (hex or name). Other valid values are `rgb`, `lab`, `lch`, `hsl`, `hsv`, `xyz`, `cmy`, and `cmyk`.\
-    \n`.extended`: Return some non-typical colorspaces in addition to the usual."
+CMD_HELP.update({"color": ["Color",
+    " - `color [options] (color)`: Use it for getting a color\n"
+    " - `.format`: Option. Format of the supplied color. Defaults to `html` which can be any valid HTML color (hex or name). Other valid values are `rgb`, `lab`, `lch`, `hsl`, `hsv`, `xyz`, `cmy`, and `cmyk`.\n"
+    " - `.extended`: Option. Return some non-typical colorspaces in addition to the usual.\n"]
 })
