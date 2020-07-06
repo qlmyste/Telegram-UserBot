9aa1a9ed7571369bca3ba1671d1bc702659abc03
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-07-06 16:35:47 +0300

    Update genius.py

--- userbot/modules/genius.py
+++ userbot/modules/genius.py
@@ -1,8 +1,6 @@
 from userbot import BOTLOG, bot, BOTLOG_CHATID, CMD_HELP, GENIUS_API
 from userbot.events import register
-from userbot.utils import parse_arguments
-from moviepy import editor as mp
-import os
+from userbot.utils import get_args_split_by, escape_html
 import lyricsgenius
 
 @register(outgoing=True, pattern=r"^\.lyrics$")
@@ -10,7 +8,7 @@ async def gen(e):
       if GENIUS_API is None:
         await e.edit("**We don't support magic! No Genius API!**")
         return
-      args = utils.get_args_split_by(e.pattern_match.group(), ",")
+      args = get_args_split_by(e.pattern_match.group(), ",")
       if len(args) != 2:
         logger.debug(args)
         await e.edit("**Syntax Error**")
@@ -23,4 +21,4 @@ async def gen(e):
       if song is None:
         await e.edit("**Can't find song**")
         return
-      await e.edit(utils.escape_html(song.lyrics))
+      await e.edit(escape_html(song.lyrics))
