a6b56f3546d8e1db94052b85b1f7d5f12f15e003
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-08-25 14:12:44 +0300

    Fix choice. Add random number from 0 to specified

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -19,15 +19,16 @@ from userbot.events import register
 async def randomise(items):
     """ For .random command, get a random item from the list of items. """
     itemo = (items.text[8:]).split()
-
+    if len(itemo) == 1:
+        await items.edit('`' +  str(int(random.random() * itemo[0])) + '`')
+        return
     if len(itemo) < 2:
         await items.edit("`2 or more items are required! Check "
                          ".help random for more info.`")
         return
 
-    index = randint(1, len(itemo) - 1)
     await items.edit("**Query: **\n`" + items.text[8:] + "`\n**Output: **\n`" +
-                     itemo[index] + "`")
+                     random.choice(itemo) + "`")
 
 
 @register(outgoing=True, pattern="^.sleep( [0-9]+)?$")
