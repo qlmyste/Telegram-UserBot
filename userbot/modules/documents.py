c9cd1e3304866818992d3967d21232e0f588c981
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-08-31 00:00:13 +0300

    Update documents.py

--- userbot/modules/documents.py
+++ userbot/modules/documents.py
@@ -8,9 +8,9 @@ from pdf2image import convert_from_path
 @register(outgoing=True, pattern=r"^\.pdf2img$")
 async def pdf(e):
   message = await e.get_reply_message()
-  test = message.file.mime_type == "application/png"
+  test = message.file.mime_type == "application/pdf"
   print(test)
-  if message.file.mime_type == "application/png":
+  if message.file.mime_type == "application/pdf":
     file = message.file
     file = await bot.download_file(file, "file.pdf")
     
