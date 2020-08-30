1c246f3ea3c04e75b4ecc26a0e225e20a7b40c30
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-08-31 00:31:23 +0300

    Update documents.py

--- userbot/modules/documents.py
+++ userbot/modules/documents.py
@@ -2,14 +2,13 @@
 # You can use this file without any permission.
 from userbot import bot, CMD_HELP
 from userbot.events import register
-import os, docx2pdf
+import os
 from pdf2image import convert_from_path
+from docx2pdf import convert
 
 @register(outgoing=True, pattern=r"^\.pdf2img$")
 async def pdf(e):
   message = await e.get_reply_message()
-  test = message.file.mime_type == "application/pdf"
-  print(test)
   if message.file.mime_type == "application/pdf":
     file = message.document
     file = await bot.download_file(file, "file.pdf")
@@ -18,4 +17,32 @@ async def pdf(e):
     images_from_path = convert_from_path('/root/Telegram-UserBot/file.pdf', output_folder='/root/Telegram-UserBot/files/', fmt='png')
     for filename in os.listdir("/root/Telegram-UserBot/files/"):
       await e.client.send_file(e.chat_id, open('/root/Telegram-UserBot/files/' + filename, 'rb'), reply_to=message)
+      rmtree("/root/Telegram-UserBot/files")
+	    os.remove(f"/root/Telegram-UserBot/file.pdf")
+      
+@register(outgoing=True, pattern=r"^\.doc2pdf$")
+async def doc(e):
+  message = await e.get_reply_message()
+  if message.file.mime_type == "application/doc" or message.file.mime_type == "application/docx":
+    file = message.document
+    file = await bot.download_file(file, "file.docx")
+    convert("file.docx", "output.pdf")
+    await e.client.send_file(e.chat_id, f'output.pdf',reply_to=message)
+    os.remove('file.docx')
+    os.remove('output.pdf')
     
+@register(outgoing=True, pattern=r"^\.doc2pdf$")
+async def doc_png(e):
+  message = await e.get_reply_message()
+  if message.file.mime_type == "application/doc" or message.file.mime_type == "application/docx":
+    file = message.document
+    file = await bot.download_file(file, "file.docx")
+    convert("file.docx", "output.pdf")
+    if os.path.isdir("/root/Telegram-UserBot/files") is False:
+      os.mkdir("/root/Telegram-UserBot/files")
+    images_from_path = convert_from_path('/root/Telegram-UserBot/output.pdf', output_folder='/root/Telegram-UserBot/files/', fmt='png')
+    for filename in os.listdir("/root/Telegram-UserBot/files/"):
+      await e.client.send_file(e.chat_id, open('/root/Telegram-UserBot/files/' + filename, 'rb'), reply_to=message)
+      rmtree("/root/Telegram-UserBot/files")
+	    os.remove(f"/root/Telegram-UserBot/output.pdf")
+      os.remove('file.docx')
