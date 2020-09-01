5cbf6c15f38df601691cd96edebd8b449c0fb5c2
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-09-01 16:08:28 +0300

    Update sst.py

--- userbot/modules/sst.py
+++ userbot/modules/sst.py
@@ -2,7 +2,7 @@ import io
 import os
 
 from google.cloud import speech_v1
-from google.cloud.speech_v1 import enums
+from google.cloud.speech_v1 import enums,types
 
 from userbot import BOTLOG, BOTLOG_CHATID, STTClient, stt, bot, CMD_HELP
 from userbot.events import register
@@ -10,10 +10,8 @@ from userbot.utils import parse_arguments
 
 DEFAULT_LANG = "en-US"
 
-
 @register(outgoing=True, pattern=r"^\.stt(\s+[\s\S]+|$)")
 async def speech_to_text(e):
-    client = speech_v1.SpeechClient()
     opts = e.pattern_match.group(1) or ""
     args, _ = parse_arguments(opts, ['lang'])
 
@@ -29,20 +27,18 @@ async def speech_to_text(e):
 
     file = await bot.download_file(file)
 
-    #content = io.BytesIO(file)
-    audio = {'content': file}
+    content = io.BytesIO(file)
+    audio = types.RecognitionAudio(content=file)
 
-    config = {
-        'encoding':enums.RecognitionConfig.AudioEncoding.OGG_OPUS,
-        'sample_rate_hertz':16000,
-        'language_code':lang
-    }
-    res = ''
-    conf = 0
-    response = client.recognize(config, audio)
-    for result in response.results:
-       alternative = result.alternatives[0]
-       res = alternative.transcript
-       conf = alternative.confidence
-    output = f"**Transcript:** {res}\n\n**Confidence:** __{round(conf, 5)}__"
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.OGG_OPUS,
+        sample_rate_hertz=16000,
+        language_code=lang
+    )
+
+    response = STTClient.long_running_recognize(config, audio)
+    op_result = response.result()
+    result = op_result.results[0].alternatives[0]
+
+    output = f"**Transcript:** {result.transcript}\n\n**Confidence:** __{round(result.confidence, 5)}__"
     await e.edit(output)
