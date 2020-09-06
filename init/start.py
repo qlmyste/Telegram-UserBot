7e3418bc13068da980461050a8de26b128cd7aa2
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-09-06 21:26:39 +0300

    Update start.py

--- init/start.py
+++ init/start.py
@@ -6,12 +6,15 @@ tf = open("./temp.txt", "r")
 isRestart = tf.read()
 isFirstBoot = True
 while isRestart == "True" or isFirstBoot:
+  print("first boot or restarted")
   if isFirstBoot:
+    print("first boot")
     isFirstBoot = False
   tf = open("./temp.txt", "r")
   isRestart = tf.read()
 
   if isRestart == "True":
+    print("restarted")
     open("./temp.txt", "w").close()
     tf = open("./temp.txt", "w+")
     tf.write("False")
