c0a94283c0f738e8a0a91dafa244182e47b5054e
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-09-06 21:49:04 +0300

    Update start.py

--- init/start.py
+++ init/start.py
@@ -4,20 +4,16 @@ tf = open("./temp.txt", "w+")
 tf.write("False")
 tf = open("./temp.txt", "r")
 isRestart = tf.read()
-isFirstBoot = True
 #            if it True            this False. It didn't enter. HOW??????
 while (isRestart == "True") or isFirstBoot:
   #making    it   False means shutdown. Works - do not touch.
   os.system("redis-server --daemonize yes")
   os.system("python3 -m userbot")
-  print("first boot or restarted")
   if isFirstBoot:
-    print("first boot")
     isFirstBoot = False
   tf = open("./temp.txt", "r")
   isRestart = tf.read()
-  print("isrestart = " + isRestart)
-  print(isRestart == "False")
+
 
 
 
