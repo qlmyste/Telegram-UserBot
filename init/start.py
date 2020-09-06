f33ad768098c19e6d20ba1d0f65a3dfcbe0df2d8
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-09-06 21:20:02 +0300

    Update start.py

--- init/start.py
+++ init/start.py
@@ -6,12 +6,13 @@ tf = open("./temp.txt", "r")
 isRestart = tf.read()
 isFirstBoot = True
 while isRestart == "True" or isFirstBoot:
-  os.system("redis-server --daemonize yes")
-  os.system("python3 -m userbot")
-  isFirstBoot = False
+  if isFirstBoot:
+    isFirstBoot = False
   tf = open("./temp.txt", "r")
   if isRestart == "True":
     open("./temp.txt", "w").close()
     tf = open("./temp.txt", "w+")
     tf.write("False")
   isRestart = tf.read()
+  os.system("redis-server --daemonize yes")
+  os.system("python3 -m userbot")
