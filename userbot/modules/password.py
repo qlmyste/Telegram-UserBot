fc7ebe8418637b36eddf7e7f856bf3f9cf471d00
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-11-15 16:30:47 +0200

    initial release

---
+++ userbot/modules/password.py
@@ -0,0 +1,12 @@
+# Module developed by Oleh Polisan
+# You can use this file without any permission.
+from userbot import BOTLOG, bot, BOTLOG_CHATID, CMD_HELP
+from userbot.events import register
+from userbot.utils import parse_arguments
+import string, random
+
+@register(outgoing=True, pattern=r"^\.password (.*)")
+async def pass(e):
+  chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+  query = await e.pattern_match.group(1)
+  print(query)
