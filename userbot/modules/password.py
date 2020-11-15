fdc2dd46208a73fdb0bee482b672f4856d5766f1
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-11-15 16:41:28 +0200

    Update password.py

--- userbot/modules/password.py
+++ userbot/modules/password.py
@@ -4,9 +4,11 @@ from userbot import BOTLOG, bot, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 from userbot.utils import parse_arguments
 import string, random
+import re
 
 @register(outgoing=True, pattern="^.password(?: |$)(.*)")
 async def password(e):
   chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
   query = e.pattern_match.group(1)
-  print(query)
+  size = re.findall(r'\d+', query)
+  print(size[0])
