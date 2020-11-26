6925b7f3a22419b054a6d1cd79cc7583dcb29c07
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-11-27 00:38:37 +0200

    Update spotify.py

--- userbot/modules/spotify.py
+++ userbot/modules/spotify.py
@@ -81,23 +81,11 @@ async def update_spotify_info():
     
     
     while SPOTIFYCHECK:
-        #print("start sp: " + str(SPOTIFYCHECK))
-        if environ.get("isSuspended") == "True":
-          try:
-              #print("isSuspended: " + environ.get("isSuspended"))
-              if isDefault == False:
-                await sleep(2)
-                await bot(UpdateProfileRequest(about=DEFAULT_BIO))
-                #print("setted default")
-                isDefault = True
-              await sleep(2)
-              continue
           except errors.FloodWaitError as e:
               await sleep(e.seconds)
               if isDefault == False:
                 await bot(UpdateProfileRequest(about=DEFAULT_BIO))
-                isDefault = True
-              continue
+                isDefault = Tr
         isGetted = False
         
         if isDefault == True:
