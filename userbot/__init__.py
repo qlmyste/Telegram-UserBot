998a9f21c9b2858a0f0c521f40c432aae290a3db
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-11-26 13:41:14 +0200

    add bg_default

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -123,6 +123,9 @@ CONVERT_TOKEN = os.environ.get("CONVERT_API", None)
 # wolframalpha.com
 WOLFRAM_ID= os.environ.get("WOLFRAM_ID", None)
 
+# wolframalpha.com
+BG_DEFAULT= os.environ.get("BG_DEFAULT", None)
+
 STRING_SESSION = os.environ.get("STRING_SESSION") or None #means heroku
 
 if STRING_SESSION:
