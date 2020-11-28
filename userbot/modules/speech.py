6a4f0a2dff9d839f704dcf4da40c88d9d350d546
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-11-29 00:26:14 +0200

    Update speech.py

--- userbot/modules/speech.py
+++ userbot/modules/speech.py
@@ -155,8 +155,10 @@ def megre_sounds(audio_file, second_file = "r"):
     new_name = "new_" + audio_file
 
     #merge
-    duration_audio=int(os.popen(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {audio_file}').read().split('.')[0])
-    duration_stock=int(os.popen(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 media/{second_file}.ogg').read().split('.')[0])
+    #duration_audio=int(os.popen(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {audio_file}').read().split('.')[0])
+    duration_audio=int(os.popen(f'ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {audio_file}').read().split('.')[0])
+    #duration_stock=int(os.popen(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 media/{second_file}.ogg').read().split('.')[0])
+    duration_stock=int(os.popen(f'ffprobe -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 media/{second_file}.ogg').read().split('.')[0])
     if(duration_audio <= duration_stock):
         os.system(f'ffmpeg -filter_complex "amovie={audio_file} [a0]; amovie=media/{second_file}.ogg [a1]; [a0][a1] amix=inputs=2:duration=shortest [aout]" -map [aout] -acodec libopus -f ogg {new_name}')
     else:
