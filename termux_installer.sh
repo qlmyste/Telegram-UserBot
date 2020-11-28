92d79bd3e57c1db61b8a7cede4a1d4df219b6f3d
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-11-28 20:21:59 +0200

    Update termux_installer.sh

--- termux_installer.sh
+++ termux_installer.sh
@@ -5,13 +5,13 @@ pip install -r requirements.txt
 pip install --upgrade --force-reinstall dnspython
 sed -i 's:/etc/resolv.conf:/data/data/com.termux/files/usr/etc/resolv.conf:g' /data/data/com.termux/files/usr/lib/python3.8/site-packages/dns/resolver.py
 
-export REPO_ROOT=grpc
-git clone https://github.com/grpc/grpc $REPO_ROOT
-cd $REPO_ROOT
-git submodule update --init
-pip install -r requirements.txt
+#export REPO_ROOT=grpc
+#git clone https://github.com/grpc/grpc $REPO_ROOT
+#cd $REPO_ROOT
+#git submodule update --init
+#pip install -r requirements.txt
 #detele std=..
-sed -i 's:-std=gnu99: ' /data/data/com.termux/files/root/Telegram-UserBot/$REPO_ROOT/setup.py
-GRPC_PYTHON_BUILD_WITH_CYTHON=1 pip install .
+#sed -i 's:-std=gnu99: ' /data/data/com.termux/files/root/Telegram-UserBot/$REPO_ROOT/setup.py
+#GRPC_PYTHON_BUILD_WITH_CYTHON=1 pip install .
 echo "Now you must edit your config.env file and start it via ./init/start.sh"
 
