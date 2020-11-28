e034d565154daecabf59c81ae367793aa49d85ad
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-11-28 23:46:19 +0200

    Update termux_installer.sh

--- termux_installer.sh
+++ termux_installer.sh
@@ -4,15 +4,5 @@ pip install cffi
 pip install -r requirements.txt 
 pip install --upgrade --force-reinstall dnspython
 sed -i 's:/etc/resolv.conf:/data/data/com.termux/files/usr/etc/resolv.conf:g' /data/data/com.termux/files/usr/lib/python3.8/site-packages/dns/resolver.py
-
-#downloading grpcio, because most probably that installing from requirements are failed because of sources problem. Fixing now.
-pip download grpcio -d grpcio
-cd grpcio
-tar -xf  grpcio*
-cd $(ls -d */|head -n 1)
-sed -i 's/-std=gnu99/ /g' ./setup.py
-python setup.py install
-cd ..
-rm -rf grpcio*
 echo "Now you must edit your config.env file and start it via ./init/start.sh"
 
