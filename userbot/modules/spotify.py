07ca66bd2629fbadfd499a40b5673afa4bed82ac
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2020-12-17 14:09:09 +0200

    Update spotify.py

--- userbot/modules/spotify.py
+++ userbot/modules/spotify.py
@@ -322,7 +322,7 @@ async def show_song(song_info):
           system(f"wget -q -O 'preview.jpg' {preview_url}")
         str_song = "Now playing: "
         if isGetted:
-          if (artist == "" || artist == '' || artist == ' ' || artist == " "):
+          if (artist == "" and artist == '' and artist == ' ' and artist == " "):
             isArtist = False
           if isArtist:
             str_song += '`' + artist + " - " + song + '`'
