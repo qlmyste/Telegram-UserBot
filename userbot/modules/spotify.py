5f4e121c375b5c9bce251067b0b2b8922051da05
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2021-01-10 18:53:02 +0200

    Update spotify.py

--- userbot/modules/spotify.py
+++ userbot/modules/spotify.py
@@ -362,7 +362,7 @@ async def show_song(song_info):
             str_song += f"\n[YouTube link]({url_yt})"
             print(str_song)
             if preview_url !="": #means NOT LOCAL song in spotify and means that there are preview
-              await telethon.client.edit(text = str_song, message = msg_to_edit)
+              await telethon.client.edit_message(text = str_song, message = msg_to_edit)
               print("must be edited.")
             else: #fetching preview from yt
               print("fetching preview from yt")
